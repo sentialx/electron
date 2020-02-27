0dad62ea6c01e703ad9d81ecfe1dd5c4432bb8b4
Shelley Vohr <shelley.vohr@gmail.com>
2020-02-27 09:04:38 -0800

    fix: bail early if no printers on the network

--- shell/browser/api/electron_api_web_contents.cc
+++ shell/browser/api/electron_api_web_contents.cc
@@ -1765,6 +1765,13 @@ void WebContents::OnGetDefaultPrinter(
 
   base::string16 printer_name =
       device_name.empty() ? default_printer : device_name;
+
+  // If there are no valid printers available on the network, bail.
+  if (printer_name.empty() || !IsDeviceNameValid(printer_name)) {
+    std::move(print_callback).Run(false, "no valid printers available");
+    return;
+  }
+
   print_settings.SetStringKey(printing::kSettingDeviceName, printer_name);
 
   auto* print_view_manager =
